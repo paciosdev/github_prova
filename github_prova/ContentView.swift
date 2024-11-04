091712af4a08eec8fa2479a9d21bf93f3a601daf
Francesco Paciello <kekkopacio@gmail.com>
2024-11-04 15:18:44 +0100

    Initial Commit

---
+++ github_prova/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  github_prova
+//
+//  Created by Francesco Paciello on 04/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ github_prova/github_provaApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_provaApp.swift
+//  github_prova
+//
+//  Created by Francesco Paciello on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct github_provaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
